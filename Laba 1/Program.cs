3bd4d896377043e15c1c6815b719d287bb6bea28
Богдан Македонський <makedonskyi.b@donnu.edu.ua>
2020-09-16 19:24:36 +0300

    Добавьте файлы проекта.

---
+++ Laba 1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Laba_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Tom";
+            int age = 33;
+            bool isEmployed = false;
+            double weight = 78.65;
+
+            Console.WriteLine($"имя:{name}");
+            Console.WriteLine($"возраст:{age}");
+            Console.WriteLine($"вес: {weight}");
+            Console.WriteLine($"работает: {isEmployed}");
+
+            Console.WriteLine("имя : {0}, возраст - {1}", name, age);
+            Console.Write("Введите имя: ");
+            string name2 = Console.ReadLine();
+            Console.WriteLine($"привет {name2}");
+
+
+            Console.Write("Введите возраст ");
+            int age2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"взраст - {age2}");
+
+            Console.Write("Введите рост: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите размер зарплаты: ");
+            decimal salary = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine($"Имя: {name} Возраст: {age} Рост: {height} cм Зарплата: {salary}$");
+
+        }
+    }
+}
